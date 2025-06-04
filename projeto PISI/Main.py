4cd7235e3483b61e5b5fe0360f1485205f08e842
Maria Eduarda <mariaeduardaalves0615@gmail.com>
2025-06-03 22:54:36 -0300

    git push

--- projeto PISI/Main.py
+++ projeto PISI/Main.py
@@ -1,22 +1,9 @@
 # CRUD
 import os
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 from validadores import *
 ARQUIVO_TXT = "usuarios.txt"
 ARQUIVO_TXT1 = "shoutbox.txt"
 ARQUIVO_TXT2 = "avaliações.txt"
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-from validadores import (email_valido, nome_valido, senha_valida)
-ARQUIVO_TXT = 'usuarios.txt'
-ARQUIVO_TXT1 = 'shoutbox.txt'
-ARQUIVO_TXT2 = 'avaliações.txt'
->>>>>>> Stashed changes
 from requisitosFuncionais import menu_funcionalidades
 
 def limpar_terminal():
